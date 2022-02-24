c79b36d1453a558108944189612687bdde3e5e86
Jose Angel <jozcob.hc6@gmail.com>
2022-02-23 23:48:46 -0500

    modularización de componentes con los decoradores in-out en jerarquia e inicio de programación reactiva

--- Store/src/app/pages/products/product/product.component.ts
+++ Store/src/app/pages/products/product/product.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Product } from '../interfaces/product.interface';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+//Con el decorador input se establece la comunicación de un componente padre a un componente hijo
+@Input() product!: Product;
+//Con el decorador output se establece la comunicación inversa
+@Output() addToCartClick = new EventEmitter<Product>();
 
-  constructor() { }
+constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
 
+    }
+
+    //Metodo personalizado para agregar al shopping card
+    onClick(): void {
+        this.addToCartClick.emit(this.product);
+    }
 }
